e36fa42080d705a615852a09ab63e83354419556
Jeff Van Rees <jeff@jvanrees.com>
2025-09-10 19:57:58 -0600

    3 image project pages transferred

--- src/routes/nightmode.tsx
+++ src/routes/nightmode.tsx
@@ -1,16 +1,33 @@
 import { createFileRoute } from '@tanstack/react-router'
-import DescriptionCard from '../components/DescriptionCard'
+import ProjectPage from '../components/ProjectPage'
+
+import img2 from '../img/night_mode/Screenshot_20181130-023540.png'
+import img3 from '../img/night_mode/Screenshot_20181130-023613.png'
+import img4 from '../img/night_mode/Screenshot_20181130-023746.png'
+import img5 from '../img/night_mode/Screenshot_20181130-023757.png'
+import img6 from '../img/night_mode/Screenshot_20181130-023810.png'
+import img1 from '../img/night_mode/Screenshot_20181130-023817.png'
+
+const images = [img1, img2, img3, img4, img5, img6]
+const title = 'New MapQuest Night Navigation Mode'
+// const paragraphs = [
+//     `Designed for night navigation, MapQuest’s new night map style provides both an aesthetically pleasing form and functional design. The needs of the user were paramount in the design process which lead us to a simplified and focused experience.`,
+//     `The process began with a competitive analysis to highlight areas of improvement in our current night style. In addition, harmony between our current day style and new night mode was prioritized for smooth transitions and consistent branding between map styles. However, the complexity of the map was reduced compared to the day map to reduce distractions during night navigation.`
+// ]
+
+const paragraphs = [
+    `MapQuest’s new night map style balances aesthetics and function, creating a simplified, focused experience for safe night navigation.`,
+    `We refined the style through competitive analysis, ensuring harmony with the day map for brand consistency while reducing complexity to minimize distractions at night.`
+];
+
+
 export const Route = createFileRoute('/nightmode')({
     component: NightmodeComponent,
 })
-const pageConfig = {
-    title: "Nightmode",
-    content: "Nightmode Placeholder"
-}
 function NightmodeComponent() {
     return (
         <div style={{ viewTransitionName: 'NightMode', height: '100%', overflowY: 'auto' }}>
-            <DescriptionCard {...pageConfig} />
+            <ProjectPage title={title} images={images} paragraphs={paragraphs} />
         </div>
     )
 }
--- src/routes/shaded-relief.tsx
+++ src/routes/shaded-relief.tsx
@@ -1,18 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
-import DescriptionCard from '../components/DescriptionCard'
+import ProjectPage from '../components/ProjectPage'
+
+import img4 from '../img/shaded_relief/finished_bike_map.png'
+import img2 from '../img/shaded_relief/shaded_dem.png'
+import img3 from '../img/shaded_relief/shaded_dom_classification.png'
+import img1 from '../img/shaded_relief/shaded_relief_cropped.png'
+
+const images = [img1, img2, img3, img4]
+const title = 'Shaded Relief'
+const paragraphs = [
+    `Cartography is a blend of art and information communication. The classic shaded relief, pioneered by Eduard Imhof, is an exemplary example of this.`,
+    `Here, I used GIS tools, Photoshop, and Illustrator to create my take on a classic. The end result was a map for a hypothetical bike race that would tour our beautiful state.`
+]
 
 export const Route = createFileRoute('/shaded-relief')({
     component: ShadedReliefComponent,
 })
-const pageConfig = {
-    title: "Shaded Relief",
-    content: "Shaded Relief Placeholder"
-}
 
 function ShadedReliefComponent() {
     return (
         <div style={{ viewTransitionName: 'ShadedRelief', height: '100%', overflowY: 'auto' }}>
-            <DescriptionCard {...pageConfig} />
+            <ProjectPage title={title} images={images} paragraphs={paragraphs} />
         </div>
     )
 }
