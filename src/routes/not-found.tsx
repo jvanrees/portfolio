5852d9525d7dcdce45e95fe6b8acda727c81d065
Jeff Van Rees <jeff@jvanrees.com>
2025-09-18 16:35:44 -0600

    404 route with cool image added

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -1,5 +1,6 @@
-import { Button, Group, Image, Title } from "@mantine/core";
+import { Button, Group, Image, Text, Title } from "@mantine/core";
 import { createFileRoute, Link } from "@tanstack/react-router";
+import notFoundSvg from "../img/404.svg";
 import logoRow from "../img/logo_row.svg";
 import aboutClasses from "../styles/About.module.css";
 import gridClasses from "../styles/Grid.module.css";
@@ -21,20 +22,38 @@ function NotFoundComponent() {
 			}}
 		>
 			<div
-				className={aboutClasses.aboutMeOpen + " " + gridClasses.subGridChild}
+				className={`${aboutClasses.aboutMeOpen} ${gridClasses.subGridChild}`}
 			>
 				<div
-					className={
-						aboutClasses.aboutMeOpenContent + " " + gridClasses.subGridChild
-					}
+					className={`${aboutClasses.aboutMeOpenContent} ${gridClasses.subGridChild}`}
+					style={{ height: "100%" }}
 				>
 					<div className={aboutClasses.mapLogoRow}>
 						<Image w={"16rem"} src={logoRow} alt="Jeff Van Rees Map Logo." />
 					</div>
-					<div className={aboutClasses.aboutMeText}>
-						<Title order={3} className={aboutClasses.aboutMeSubTitles}>
-							404 - Page Not Found
+					<div
+						className={aboutClasses.aboutMeText}
+						style={{
+							height: "100%",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "center",
+							textAlign: "center",
+						}}
+					>
+						<Image
+							w={"45rem"}
+							src={notFoundSvg}
+							alt="404 Not Found Illustration"
+							radius={"md"}
+							style={{ marginBottom: "1rem" }}
+						/>
+
+						<Title order={1} className={aboutClasses.aboutMeSubTitles}>
+							404 Destination Not Found
 						</Title>
+						<Text>You've gone off the map. Recalculating route...</Text>
 					</div>
 				</div>
 			</div>
@@ -42,16 +61,9 @@ function NotFoundComponent() {
 			<div
 				className={
 					isMobile
-						? aboutClasses.footer + " " + aboutClasses.buttonContainerShadow
-						: aboutClasses.buttonContainerBottom +
-							" " +
-							aboutClasses.white +
-							" " +
-							aboutClasses.buttonContainerShadow +
-							" " +
-							aboutClasses.buttonDesktopBack
+						? `${aboutClasses.footer} ${aboutClasses.buttonContainerShadow}`
+						: `${aboutClasses.buttonContainerBottom} ${aboutClasses.white} ${aboutClasses.buttonContainerShadow} ${aboutClasses.buttonDesktopBack}`
 				}
-				style={{ height: "100%" }}
 			>
 				<Group>
 					<Button component={Link} to="/" leftSection="←" variant="transparent">
