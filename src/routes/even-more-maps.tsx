4a82b419108b8b90f3352a39bee9706e42ce298c
Jeff Van Rees <jeff@jvanrees.com>
2025-09-23 21:34:52 -0600

    added flyto effect to bg map for more maps route

--- src/routes/even-more-maps.tsx
+++ src/routes/even-more-maps.tsx
@@ -1,19 +1,49 @@
 import { Accordion, Text } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 import clsx from "clsx";
-import { useState } from "react";
-import { Layer, Source } from "react-map-gl/maplibre";
+import { easeSinInOut } from "d3-ease";
+import { useEffect, useState } from "react";
+import { Layer, Source, type ViewState } from "react-map-gl/maplibre";
 import { EvenMoreMapsItems } from "../components/EvenMoreMapsItems";
 import ProjectPageBottomNav from "../components/ProjectPageBottomNav";
 import ProjectPageMap from "../components/ProjectPageMap";
+import { type MapColor, useMapContext } from "../context/mapstyleContext";
 import styles from "../styles/ProjectPage.module.css";
 
 export const Route = createFileRoute("/even-more-maps")({
 	component: EvenMoreComponent,
 });
 
+const viewport: ViewState = {
+	latitude: 40.32,
+	longitude: -94.34,
+	bearing: 0,
+	pitch: 0,
+	zoom: 3.35,
+	padding: { top: 0, bottom: 0, left: 0, right: 0 },
+};
+
+const mapColor: MapColor = {
+	background: "hsla(80, 11%, 95%, 1.00)",
+	park: "hsla(50, 94%, 69%, 0.00)",
+	water: "hsla(210, 94%, 93%, 1.00)",
+	roadLow: "hsla(49, 93%, 68%, 0.00)",
+	roadHigh: "hsla(49, 93%, 68%, 0.00)",
+	mapOverlayOpacity: 0.09,
+};
+
+const flyToDuration = 2000;
+const easing = easeSinInOut;
+
 function EvenMoreComponent() {
 	const [activeKey, setActiveKey] = useState("1");
+	const { flyToViewport, setMapColor, isMapLoaded } = useMapContext();
+	useEffect(() => {
+		if (isMapLoaded) {
+			flyToViewport(viewport, flyToDuration, easing);
+			setMapColor(mapColor);
+		}
+	}, [flyToViewport, setMapColor, isMapLoaded]);
 
 	const handleActiveChange = (value: string | null) => {
 		if (value) {
